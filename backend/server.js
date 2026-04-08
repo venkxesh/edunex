383a477e3545066a88fa74f989923926707d5cb5
venkxesh <venkatesh292010@gmail.com>
2026-04-09 05:10:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const connectDB = require("./config/db");
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 connectDB();
 
 // middleware
+app.use(cors()); // 🔥 IMPORTANT
 app.use(express.json());
 
 // routes
@@ -15,6 +18,8 @@ const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
 
 // server start
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
